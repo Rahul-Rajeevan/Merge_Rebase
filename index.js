47afe969d3ed47a6f3593da39a63b8720ff76658
rahul-rajeevan <rahul.rajeevan4@gmail.com>
2025-04-05 15:36:07 +0530

    main branch com

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Data Encapsulation: As shown, you can create “private” variables. For instance, 
-// a module pattern or factory function can use closures to store internal state and 
-// expose only certain functions. e.g., a function createUser(name) that internally stores name 
-// and returns methods like getName() or setName() – those methods form closures over the name variable, 
-// keeping it hidden from the outside except through those methods.
-
-
+function abc(){
+    
+}
